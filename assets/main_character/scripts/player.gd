be8910bc6984586c9708988c70fc3022a5f5258a
viniciusagvaz <agvazvinicius@gmail.com>
2023-11-07 21:47:16 -0300

    animation tree

--- assets/main_character/scripts/player.gd
+++ assets/main_character/scripts/player.gd
@@ -1,13 +1,22 @@
 extends CharacterBody2D
 
+var _state_machine
+
 @export_category("Variables")
 @export var _move_speed: float = 64.0
 
-@export var _friction: float = 0.8
+@export var _friction: float = 0.4
 @export var _acceleration: float = 0.4
+
+@export_category("Objects")
+@export var _animation_tree: AnimationTree = null
+
+func _ready() -> void:
+	_state_machine = _animation_tree["parameters/playback"]
 
 func _physics_process(_delta: float) -> void:
 	_move()
+	_animate()
 	move_and_slide()
 
 
@@ -17,12 +26,21 @@ func _move():
 		Input.get_axis("move_up", "move_down")
 	)
 	
-	if _direction != Vector2.ZERO:
+	if _direction !=  Vector2.ZERO:
+		_animation_tree["parameters/idle/blend_position"] = _direction
+		_animation_tree["parameters/walk/blend_position"] = _direction
+		
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
 		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
 		return
-		
+
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
 
 
+func _animate():
+	if velocity.length() > 5:
+		_state_machine.travel("walk")
+		return
+		
+	_state_machine.travel("idle")
